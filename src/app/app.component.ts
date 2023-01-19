ca31246e25e261d70705420e3f3f354d94f51f7a
Dulce Carolina Martínez Carrera <dulce.martinez@ulsaoaxaca.edu.mx>
2023-01-19 17:00:56 -0600

    Clase Google Analytics y Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators'
+
+declare var gtag;
 
 @Component({
   selector: 'app-root',
@@ -6,4 +10,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(
+    private ruoter: Router
+  ){
+      const  navEndEvent$ = this.ruoter.events
+                            .pipe(
+                                filter( event => event instanceof NavigationEnd)
+                            );
+      navEndEvent$.subscribe( (event: NavigationEnd) => {
+        gtag('config', 'G-3EDX31KQ83',{
+          page_path: event.urlAfterRedirects
+        });
+      });
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  url_api: 'https://store.nicobytes.site/api',
+  url_api: 'https://young-sands-07814.herokuapp.com/api',
   firebase: {
     apiKey: 'AIzaSyCN599W6_flBEpe0xrcuBlGPRW3OKzDr7o',
     authDomain: 'platzi-store-forms.firebaseapp.com',
